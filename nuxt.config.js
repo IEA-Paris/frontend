b71c71cb6c28977fd02dafac226fa204e53a8979
Billybobbonnet <boitalettre9@gmail.com>
2021-08-24 19:01:43 +0200

    attempt to fix CI :'(

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   /* ssr: false, */
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  ssr: false,
   router: {
     base: '/frontend/',
   },
